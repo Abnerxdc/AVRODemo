02963a8cc49dccbb176a6c484bd2df06169e0fb3
Abnerxdc <1140271523@qq.com>
2017-12-21 13:44:05 +0800

    测试git

--- src/main/java/avro/AvroUtils.java
+++ src/main/java/avro/AvroUtils.java
@@ -97,7 +97,7 @@ public class AvroUtils {
     /**
      * 序列化逻辑（简单版）
      * @param schema 规则
-     * @param map 数据AA
+     * @param map 数据BB
      * @return 序列化后的数据
      */
     public static byte[] getSerizilizeSimple(Schema schema , Map<String,String> map) {
