f8de2e3d9a2baa1070fe5e710445f3730c0224ad
Abnerxdc <1140271523@qq.com>
2017-12-29 17:22:08 +0800

    测试git

--- src/main/java/avro/AvroUtils.java
+++ src/main/java/avro/AvroUtils.java
@@ -8,10 +8,12 @@ import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.*;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -20,23 +22,23 @@ import java.util.*;
  */
 public class AvroUtils {
     public static String getSchemaDefine() {
-        String schemaStr = "{\"namespace\":\"datatype.avro\",\"type\":\"record\",\"name\":\"RecordList\",\"fields\":[{\"name\":\"schema_16\",\"type\":[{\"type\":\"array\",\"items\":{\"namespace\":\"datatype.avro\",\"type\":\"record\",\"name\":\"DATA_DNSRecord\",\"fields\":[{\"name\":\"CDR_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"}, {\"name\":\"LOCATION_CODE\",\"type\":[ \"string\",\"null\"],\"default\":\"\"},{\"name\":\"SYSTEM_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"}]}}]}]}";
+        String schemaStr = "{\"namespace\":\"datatype.avro\",\"type\":\"record\",\"name\":\"RecordList\",\"fields\":[{\"name\":\"schema_16\",\"type\":[{\"type\":\"array\",\"items\":{\"namespace\":\"datatype.avro\",\"type\":\"record\",\"name\":\"DATA_DNSRecord\",\"fields\":[{\"name\":\"CDR_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"}, {\"name\":\"LOCATION_CODE\",\"type\":[ \"string\",\"null\"],\"default\":\"\"},{\"name\":\"SYSTEM_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"},{\"name\":\"HOST_IP\",\"type\":[{\"type\":\"array\",\"items\":\"string\"},\"null\"],\"default\":\"\"}]}}]}]}";
         return schemaStr;
     }
     public static String getChildSchemaDefine() {
-        String schemaStr = "{\"namespace\":\"datatype.avro\",\"type\":\"record\",\"name\":\"DATA_DNSRecord\",\"fields\":[{\"name\":\"CDR_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"}, {\"name\":\"LOCATION_CODE\",\"type\":[ \"string\",\"null\"],\"default\":\"\"},{\"name\":\"SYSTEM_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"}]}";
+        String schemaStr = "{\"namespace\":\"datatype.avro\",\"type\":\"record\",\"name\":\"DATA_DNSRecord\",\"fields\":[{\"name\":\"CDR_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"}, {\"name\":\"LOCATION_CODE\",\"type\":[ \"string\",\"null\"],\"default\":\"\"},{\"name\":\"SYSTEM_ID\",\"type\":[\"string\",\"null\"],\"default\":\"\"},{\"name\":\"HOST_IP\",\"type\":[{\"type\":\"array\",\"items\":\"string\"},\"null\"],\"default\":\"\"}]}";
         return schemaStr;
     }
     public static void main(String[] args) {
         String rawSchemaName = "schema_16";
-        Map<String,String> map1 = new HashMap<String,String>();
+        Map<String,String> map1 = new HashMap<String,String>(3);
         map1.put("CDR_ID","123");
         map1.put("LOCATION_CODE","234");
         map1.put("SYSTEM_ID","999");
-        Map<String,String> map2 = new HashMap<String,String>();
+        Map<String,String> map2 = new HashMap<String,String>(3);
         map2.put("CDR_ID","1234");
         map2.put("LOCATION_CODE","2345");
-        map2.put("SYSTEM_ID","9999");
+//        map2.put("SYSTEM_ID","9999");
         List<Map<String,String>> list = new ArrayList<>();
         list.add(map1);
         list.add(map2);
@@ -50,8 +52,84 @@ public class AvroUtils {
         GenericData.Array<GenericData.Record> array = parseInnerRecordArray(records,rawSchemaName);
         System.out.println(array.get(0).toString());
         System.out.println(array.get(1).toString());
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        byte[] valueBytes1 = getSerizilizeMiddle(new Schema.Parser().parse(getChildSchemaDefine()),list);
+        //反序列化逻辑
+        List<GenericRecord> record1 = getDeserializeIterator(valueBytes1,new Schema.Parser().parse(getChildSchemaDefine()));
+        System.out.println(record1);
+
+        writeFile(new Schema.Parser().parse(getChildSchemaDefine()),list);
+        System.out.println("=======================================================");
+        byte[] a = serialize(list , new Schema.Parser().parse(getSchemaDefine()),new Schema.Parser().parse(getChildSchemaDefine()),"schema_16");
+        System.out.println(a);
+        System.out.println(getAvroDe_9(valueBytes, new Schema.Parser().parse(getSchemaDefine())));
+        System.out.println(getAvroDe_9(a,new Schema.Parser().parse(getSchemaDefine())));
     }
 
+    /**
+     * 序列化逻辑（简单版）
+     * @param schema 规则
+     * @param map 数据
+     * @return 序列化后的数据
+     */
+    public static byte[] getSerizilizeSimple(Schema schema , Map<String,String> map) {
+        byte[] avroValusStr = null;
+        GenericData.Record record = new GenericData.Record(schema);
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> mapEntry = (Map.Entry) it.next();
+            String key = mapEntry.getKey();
+            String value = mapEntry.getValue();
+            record.put(key, value);
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DatumWriter<GenericRecord> datumWrite = new GenericDatumWriter<GenericRecord>(schema);
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWrite);
+        try {
+            dataFileWriter.create(schema, outputStream);
+            dataFileWriter.append(record);
+            dataFileWriter.close();
+            avroValusStr = outputStream.toByteArray();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return avroValusStr;
+    }
+    /**
+     * 序列化逻辑（中间版）
+     * @param schema 规则
+     * @param list 数据
+     * @return 序列化后的数据
+     */
+    public static byte[] getSerizilizeMiddle(Schema schema , List<Map<String,String>> list) {
+        byte[] avroValusStr  = null;
+
+        DatumWriter<GenericRecord> datumWrite = new GenericDatumWriter<GenericRecord>(schema);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWrite);){
+            dataFileWriter.create(schema, outputStream);
+            for(int i=0;i<list.size();i++){
+                Map<String,String> map = list.get(i);
+                GenericData.Record record = new GenericData.Record(schema);
+                Iterator it = map.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> mapEntry = (Map.Entry) it.next();
+                    String key = mapEntry.getKey();
+                    String value = mapEntry.getValue();
+                    record.put(key, value);
+                }
+                dataFileWriter.append(record);
+            }
+            dataFileWriter.close();
+            avroValusStr = outputStream.toByteArray();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return avroValusStr;
+    }
     /**
      * 序列化逻辑（复杂版）
      * @param rawSchemaName
@@ -80,42 +158,11 @@ public class AvroUtils {
         }
         parentRecord.put(rawSchemaName,records);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        DatumWriter<GenericRecord> datumWrite = new GenericDatumWriter<GenericRecord>(new Schema.Parser().parse(getSchemaDefine()));
+        DatumWriter<GenericRecord> datumWrite = new GenericDatumWriter<GenericRecord>(parentSchema);
         DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWrite);
         try {
             dataFileWriter.create(parentSchema, outputStream);
             dataFileWriter.append(parentRecord);
-            dataFileWriter.close();
-            avroValusStr = outputStream.toByteArray();
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return avroValusStr;
-    }
-
-    /**
-     * 序列化逻辑（简单版）
-     * @param schema 规则
-     * @param map 数据
-     * @return 序列化后的数据
-     */
-    public static byte[] getSerizilizeSimple(Schema schema , Map<String,String> map) {
-        byte[] avroValusStr = null;
-        GenericData.Record record = new GenericData.Record(schema);
-        Iterator it = map.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, String> mapEntry = (Map.Entry) it.next();
-            String key = mapEntry.getKey();
-            String value = mapEntry.getValue();
-            record.put(key, value);
-        }
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        DatumWriter<GenericRecord> datumWrite = new GenericDatumWriter<GenericRecord>(new Schema.Parser().parse(getSchemaDefine()));
-        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWrite);
-        try {
-            dataFileWriter.create(schema, outputStream);
-            dataFileWriter.append(record);
             dataFileWriter.close();
             avroValusStr = outputStream.toByteArray();
             outputStream.close();
@@ -139,13 +186,36 @@ public class AvroUtils {
             DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(seekableFileInput, datumReader);
             while (dataFileReader.hasNext()) {
                 record = dataFileReader.next(record);
+                System.out.println(">>>>>>>>>>>>>>>"+record);
             }
         }catch (Exception e){
             e.printStackTrace();
         }
         return record;
     }
-
+    /**
+     * 反序列化逻辑
+     * @param serilizedValue 序列化之后的数据
+     * @param schema 序列化规则
+     * @return 反序列化后的数据
+     */
+    public static List<GenericRecord> getDeserializeIterator(byte[] serilizedValue,Schema schema){
+        List<GenericRecord> list = new ArrayList<>();
+        GenericRecord record = null;
+        SeekableByteArrayInput seekableFileInput = new SeekableByteArrayInput(serilizedValue);
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
+        try{
+            DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(seekableFileInput, datumReader);
+            while (dataFileReader.hasNext()) {
+                record = dataFileReader.next(record);
+                list.add(record);
+                System.out.println(">>>>>>>>>>>>>>>"+record);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
     /**
      * 获取 某个值后，再强制类型转化为array 每一个为Record <key,value>
      * @param genericRecord
@@ -155,5 +225,69 @@ public class AvroUtils {
     public static GenericData.Array<GenericData.Record> parseInnerRecordArray(GenericRecord genericRecord,String rawSchemaName){
         return ((GenericData.Array<GenericData.Record>) genericRecord.get(rawSchemaName));
     }
+    public static void writeFile(Schema schema,List<Map<String,String>> list){
+        try {
+            DatumWriter<GenericRecord> dwriter = new SpecificDatumWriter<GenericRecord>(schema);
+            //DatumWriter 将数据对象翻译成Encoder对象可以理解的类型，
+            DataFileWriter<GenericRecord> write=new DataFileWriter<GenericRecord>(dwriter);
+            write.create(schema,  new File("./conf/a.txt"));
+            for(int i=0;i<list.size();i++){
+                Map<String,String> map = list.get(i);
+                GenericData.Record record = new GenericData.Record(schema);
+                Iterator it = map.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> mapEntry = (Map.Entry) it.next();
+                    String key = mapEntry.getKey();
+                    String value = mapEntry.getValue();
+                    record.put(key, value);
+                }
+                write.append(record);
+            }
+            write.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    public static GenericRecord getAvroDe_9(byte[] ser , Schema schema){
+        GenericRecord record = null;
+        DatumReader<GenericRecord> datumReader = new SpecificDatumReader<>(schema);
+        try{
+            Decoder decoder = DecoderFactory.get().binaryDecoder(ser,null);
+            record = datumReader.read(null,decoder);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return record;
+    }
+    public static byte[] serialize(List<Map<String,String>> list, Schema parentSchema,Schema childSchema,String rawSchemaName) {
+
+        GenericData.Record parentRecord = new GenericData.Record(parentSchema);
+        List<GenericData.Record> records = new ArrayList<>();
+        for(int i=0; i<list.size();i++){
+            GenericData.Record record = new GenericData.Record(childSchema);
+            Map map = list.get(i);
+            Iterator it = map.entrySet().iterator();
+            while (it.hasNext()){
+                Map.Entry<String,String> mapEntry = (Map.Entry) it.next();
+                String key = mapEntry.getKey();
+                String value = mapEntry.getValue();
+                record.put(key,value);
+            }
+            records.add(record);
+        }
+        parentRecord.put(rawSchemaName,records);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        DatumWriter<GenericRecord> writer = new SpecificDatumWriter<GenericRecord>(parentSchema);
+        try {
+            writer.write(parentRecord, encoder);
+            encoder.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toByteArray();
+    }
 }
